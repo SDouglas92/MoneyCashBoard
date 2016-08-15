d853a05c387aab3acb2129f48df03f52cdbca1a8
SDouglas92 <simon.douglas2403@sky.com>
2016-08-15 13:03:46 +0100

    created index file to display all merchants

--- controllers/merchant_controller.rb
+++ controllers/merchant_controller.rb
@@ -3,9 +3,23 @@ require_relative('../models/merchant.rb')
 require_relative('../models/analysis.rb')
 
 # Index
+get '/merchants' do
+  @merchants = Merchant.all
+  erb(:'merchants/index')
+end
 
 # New
+get '/merchants/new' do
+  erb(:'merchants/new')
+end
 
 # Create
+post '/merchants' do
+
+end
 
 # Show
+
+get '/merchants/:id' do
+
+end
